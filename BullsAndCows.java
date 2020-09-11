29bf3c2d3afe918d2847fdeb7b801d283c48da14
Kushagr <kushagrarora.17@gmail.com>
2020-09-11 10:13:42 +0530

    Create BullsAndCows.java

---
+++ BullsAndCows.java
@@ -0,0 +1,60 @@
+/*
+https://leetcode.com/explore/challenge/card/september-leetcoding-challenge/555/week-2-september-8th-september-14th/3455/
+
+You are playing the following Bulls and Cows game with your friend: You write down a number and ask your friend to guess what the number is. Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret number exactly in both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position (called "cows"). Your friend will use successive guesses and hints to eventually derive the secret number.
+
+Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows. 
+
+Please note that both secret number and friend's guess may contain duplicate digits.
+
+Example 1:
+
+Input: secret = "1807", guess = "7810"
+
+Output: "1A3B"
+
+Explanation: 1 bull and 3 cows. The bull is 8, the cows are 0, 1 and 7.
+Example 2:
+
+Input: secret = "1123", guess = "0111"
+
+Output: "1A1B"
+
+Explanation: The 1st 1 in friend's guess is a bull, the 2nd or 3rd 1 is a cow.
+Note: You may assume that the secret number and your friend's guess only contain digits, and their lengths are always equal.
+*/
+
+class Solution {
+  public String getHint(String secret, String guess) {
+    int length = secret.length();
+    
+    int[] x = new int[10];
+    int[] y = new int[10];
+    // Arrays.fill(x, 1);
+    
+    int bull = 0;
+    
+    for(int i = 0; i < length; i++) {
+      int s = secret.charAt(i) - '0';
+      int g = guess.charAt(i) - '0';
+      
+      if(s == g) {
+        bull++;
+      }
+      else {
+        x[s]++;
+        y[g]++;
+      }
+    }
+    
+    int cow = 0;
+    for(int i = 0; i < 10; i++) {
+      if(y[i] != 0 && x[i] != 0) {
+        cow += Math.min(x[i], y[i]);
+      }
+    }
+    
+    
+    return bull + "A" + cow + "B";
+  }
+}
